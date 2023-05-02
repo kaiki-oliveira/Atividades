4ed9e454a32b459ec7e4fa3d847cba3881ba83ff
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2023-05-02 10:08:10 -0300

    estudo de vetores

---
+++ JavaScript/aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 3]
+num[5] = 7
+num.push(4)
+let pos = num.indexOf(9)
+console.log(num)
+console.log(pos)
---
+++ JavaScript/aula15/vetornatela.js
@@ -0,0 +1,9 @@
+let num = [1,2,3,4,5,6,7,8,9]
+/*
+for(let pos = 0; pos < num.length; pos++){
+    console.log(num[pos])
+}
+*/
+for(let pos in num){
+    console.log(num[pos])
+}
