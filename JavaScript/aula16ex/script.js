e118a77fc48ba047bb24b0b16776f52ec6efdb0d
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2023-05-04 23:01:36 -0300

    ..

---
+++ JavaScript/aula16ex/script.js
@@ -0,0 +1,18 @@
+function calcular(){
+    var n1 = window.document.getElementById('n1')
+    var tab = window.document.getElementById('tab')
+    if(n1.value.length == 0){
+        window.alert('por favor digite um dunero')
+    }
+    else{
+        var n = Number(n1.value)
+        for(var c = 1; c <= 10; c++){
+            var item = window.document.createElement('option')
+            item.text = `${n} * ${c} = ${n*c}`
+            tab.appendChild(item)
+            
+            
+        }
+    }
+     
+}
