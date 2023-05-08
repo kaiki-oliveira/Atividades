e03ee67ea9a4056f365aaff800f4135d13104c99
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2023-05-07 22:18:54 -0300

    ...

--- JavaScript/aula16ex/script.js
+++ JavaScript/aula16ex/script.js
@@ -1,17 +1,12 @@
 function adicionar(){
     var num = window.document.getElementById('txtnum')
     if(num.value.length == 0){
-        window.alert('por favor digite um dunero para continuar')
+        window.alert('por favor digite um numero para continuar')
     }
     else{
-        var n = Number(n1.value)
-        for(var c = 1; c <= 10; c++){
-            var item = window.document.createElement('option')
-            item.text = `${n} * ${c} = ${n*c}`
-            tab.appendChild(item)
-            
-            
-        }
-    }
-     
+        var n = Number(num.value)
+        var item = window.document.createElement('option')
+        item.text = n
+        tab.appendChild(item)
+    }   
 }
