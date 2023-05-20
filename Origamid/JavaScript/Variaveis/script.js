f9ddb46438d88deef131108576d103e6b78f268f
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2023-05-20 18:26:47 -0300

    ...

---
+++ Origamid/JavaScript/Variaveis/script.js
@@ -0,0 +1,39 @@
+var nome = 'kaiki'
+var idade = 19
+var possuiFaculdade = false
+
+console.log(nome, idade, possuiFaculdade, nome)
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = preco * totalComprado
+
+console.log (totalPreco)
+
+var sobrenome = 'Oliveira', cidade = 'Salvador'
+console.log(sobrenome, cidade)
+
+var semDefinir
+
+console.log(semDefinir)
+
+var comida
+console.log(comida)
+comida='pizza'
+console.log(comida)
+
+var time = 'flamengo'
+console.log(time)
+time='vitoria'
+console.log(time)
+
+var nome = 'kaiki'
+var idade = 19
+var comida
+comida = 'pizza'
+
+var time
+var cidade
+var sobrenome
+var sonho
+var dinheiro
