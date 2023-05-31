6f8efff45edddaf4bab10dd7f71c0fa0c703ddda
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2023-05-31 07:12:23 -0300

    ...

---
+++ Origamid/JavaScript/bolean-e-condicionais/script.js
@@ -0,0 +1,43 @@
+var possuiGraduacao = false
+var possuiDoutorado = true
+if (possuiGraduacao){
+    console.log('possui graduação')
+}
+else if (possuiDoutorado){
+    console.log('possui doutorado')
+}
+else{
+    console.log('não possui nada')
+}
+
+var nome = ' '
+if (nome){
+    console.log(nome)
+}
+else{
+    console.log('não tem nome')
+}
+
+if((5-5) && (5+5)){
+    conaole.log('verdadeiro')
+}
+else{
+    console.log('falso')
+}
+
+if((5-10) && (5+5)){
+    conaole.log('verdadeiro')
+}
+else{
+    console.log('falso')
+}
+
+let corFavorita = 'azul'
+switch(corFavorita){
+    case 'azul':
+        console.log(corFvorita)
+        break
+    default:
+        console.log('digite uma cor')
+        
+}
