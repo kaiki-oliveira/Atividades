10658cb60cce2b516ce5db95306c24a1cf654ae5
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2023-08-22 16:00:46 -0300

    ...

---
+++ Origamid/JavaScript/atribuicao-e-ternario/script.js
@@ -0,0 +1,25 @@
+var numero = 20
+
+numero += 10 // numero = numero + 10
+console.log(numero)
+
+
+var idade = 19
+var podeBeber = idade >= 18 ? 'pode beber': 'não pode beber'
+console.log(podeBeber)
+
+var possuiFaculdade = true
+
+if(possuiFaculdade) 
+    console.log('possui faculdade')
+else 
+    console.log('não possui faculdade')
+
+//EXERCICIO 
+
+var scroll = 1000
+scroll += 500
+
+var possuiCarroCasa = true
+var darCredito =  possuiCarroCasa = true?  'dar credito': 'não dar credito'
+
---
+++ Origamid/JavaScript/escopo/script.js
@@ -0,0 +1,17 @@
+function mostrarCarro(){
+    var carro = 'fusca'
+    console.log(carro)
+}
+
+mostrarCarro()
+console.log(carro)
+
+{
+    var mes = 'agosto'
+    console.log(mes)
+}
+
+console.log(mes)
+
+//EXERCICIO
+
