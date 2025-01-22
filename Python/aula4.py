b703247b592deb409559ea3d0688909fb493a012
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2025-01-22 08:50:36 -0300

    update

---
+++ Python/aula4.py
@@ -0,0 +1,29 @@
+"""
+Tipos int e float
+int -> numero inteiro
+o tipo int representa qualquer numero
+Positivo ou negativo. int sem sinal é considerado positivo
+
+float -> numero com ponto flutuante
+O tipo float representa qualquer numero
+Positivo ou negativo com ponto flutuante
+float sem sinal é cosiderado positivo
+
+A função type mostra a função que o python inferiu ao valor.
+"""
+
+#int
+print(11)
+print(-11)
+print(0)
+
+#float
+print(1.1)
+print(0.0)
+print(-1.5)
+
+#type
+
+print(type('kaiki'))
+print(type(1))
+print(type(1.1))
