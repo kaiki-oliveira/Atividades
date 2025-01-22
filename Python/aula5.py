4ff917d24863b32d5ece1f74d27415dd3b486f08
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2025-01-22 09:05:44 -0300

    update

---
+++ Python/aula5.py
@@ -0,0 +1,11 @@
+"""
+Tipo de dados bool (boolean)
+Ao questionar algo em um programa,
+só existem duas respostas possiveis:
+Sim (True) Não (False).
+Existem varios operadores para "questionar".
+Dentre eles o ==, que é um operador logico que
+Questiona se um valor é igual ao outro.
+"""
+print(10 == 10)
+print(10 == 11)
