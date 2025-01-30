3cf2c26f80a0cb097fa38e5970f88eac96acf107
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2025-01-30 09:19:02 -0300

    upgrade

---
+++ Python/aula7.py
@@ -0,0 +1,19 @@
+"""
+Variaveis são usadas para armazenar algo na memoria do computador
+PEP8: inicie variaveis com letras minusculas, pode usar numeros e underline.
+O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a uma variavel.
+Uso: nome_variavel = expressão.
+"""
+'''
+nome_completo = "kaiki oliveira"
+soma_dois_mais_dois = 2 + 2
+int_um = int('1')
+print(int_um, type(int_um))
+print(nome_completo, soma_dois_mais_dois)
+'''
+nome = 'kaiki'
+idade = 20
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
+
