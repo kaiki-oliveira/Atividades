80e2284fe0d4df292b31fcd6d7ee57ae3bbb13e6
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2025-02-03 08:28:36 -0300

    upgrade

---
+++ Python/aula8.py
@@ -0,0 +1,13 @@
+nome = 'kaiki'
+sobrenome = 'oliveira'
+idade = 20
+ano_nascimento = 2004
+maior_de_idade = idade >= 18
+altura_metros = 1.79
+
+print('nome:', nome)
+print('sobrenome:', sobrenome)
+print('iade:', idade)
+print('ano de nascimento:', ano_nascimento)
+print('é maior de idade?', maior_de_idade)
+print('altura em metros:', altura_metros)
