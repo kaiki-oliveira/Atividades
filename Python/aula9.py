b7efec56f4dc322803c413126e2f412483b9e847
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2025-02-12 08:49:23 -0300

    upgrade

---
+++ Python/aula9.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('adição', adicao)
+
+subtracao = 10 - 5
+print('subtração', subtracao)
+
+mutiplicacao = 10 * 10
+print('mutiplicação', mutiplicacao)
+
+divisao = 10 / 2.2
+print('divisão', divisao)
+
+divisao_inteira = 10 // 2.2
+print('divisão inteira', divisao_inteira)
+
+exponenciacao = 10 ** 10
+print('exponenciação', exponenciacao)
+
+mdulo = 55 % 2
+print('moodulo, modulo')
