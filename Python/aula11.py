a742626a2327639118e2c6131d9f1fd35cafdcd1
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2025-03-10 09:47:02 -0300

    upload

--- Python/aula11.py
+++ Python/aula11.py
@@ -1,6 +1,6 @@
 nome = 'kaiki'
 altura = 1.77
 peso = 88.44
-imc = peso / (altura * altura)
+imc = peso / altura ** 2
 
 print(nome, 'tem', altura, 'de altura, pesa', peso, 'e seu imc é', imc )
--- Python/aula13.py
+++ Python/aula13.py
@@ -1,6 +1,6 @@
 a = 'A'
 b = 'B'
 c = 1.1
-formato = ''
+formato = 'a = {nome1} b = {nome2} c = {nome3:.2f}'.format(nome1 = a, nome2 = b, nome3 = c)
 
 print(formato)
