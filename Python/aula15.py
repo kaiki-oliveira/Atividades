7ed8d79c08e10ac0c7f5d7d6909860395eea789b
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2025-03-13 07:25:42 -0300

    ...

---
+++ Python/aula15.py
@@ -0,0 +1,10 @@
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou')
+
+elif entrada == 'sair':
+    print('Você saiu')
+
+else:
+    print('Digite apenas "entrar" ou "sair"')
