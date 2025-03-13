5efc7f25c69b44aad86b6b5828cd8db5dc48ec04
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2025-03-13 07:25:33 -0300

    ...

---
+++ Python/aula14.py
@@ -0,0 +1,9 @@
+#nome = input('qual o seu nome? ')
+#print(f'olá {nome}')
+
+numero_1 = input('digite um numero: ')
+numero_2 = input('digite ouro numero: ')
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2) 
+soma = int_numero_1 + int_numero_2
+print(f'A soma dos numeros é {soma}')
