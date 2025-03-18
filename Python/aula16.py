154a90ecacabd5977042932aa07dd0d710b95ea7
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2025-03-18 06:51:44 -0300

    ...

---
+++ Python/aula16.py
@@ -0,0 +1,16 @@
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = True
+if condicao1:
+    print('esse é código da condicao1')
+elif condicao2:
+    print('esse código é da condicao2')
+elif condicao3:
+    print('esse código é da condicao3')
+elif condicao4:
+    print('esse código é da condicao4')
+else:
+    print('nenhuma condicao foi acionada')
+
+print( 'fora do if')
