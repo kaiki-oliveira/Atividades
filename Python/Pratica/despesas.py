ae7f5524950872ecb0372844818b075b213e0fb1
kaiki oliveira <103068974+kaiki-oliveira@users.noreply.github.com>
2025-03-27 13:38:11 -0300

    Update despesas.py

--- Python/Pratica/despesas.py
+++ Python/Pratica/despesas.py
@@ -1,4 +1,47 @@
-receita = input('Digite sua receita mensal:')
+valor = 0
+receita = 0
+
+while True:
+
+    print('************ MENU *************')
+    print('(1) adicionar despesa')
+    print('(2) pagar despesa')
+    print('(3) adicionar renda')
+    print('(4) mostrar renda')
+    print('(5) mostrar valor a pagar')
+    print('(6) sair')
+    print('*******************************')
+
+
+    acao = int(input('qual ação deseja realizar? '))
+
+
+    if acao == 1:
+        despesa = float(input('digite o valor que deseja adicionar: '))
+        valor += despesa
+    elif acao == 2:
+        pagamento = float(input('digite o valor que deseja pagar: '))
+        if pagamento < valor:
+            print('esse valor é menor que a sua despesa!')
+        else:
+            valor -= pagamento
+    elif acao == 3:
+        renda = float(input('digite o valor que deseja adicionar: '))
+        receita += renda
+    elif acao == 4:
+        print(f'sua receita total é: {receita}')
+    elif acao == 5:
+        print('esse é o valor da sua despesa:')
+        print(valor)
+    elif acao.lower() == 6:
+        break
+    else:
+        print('valor invalido,tente novamente')
+print('processo finalizado, volte sempre!')
+
+        
+
+'''receita = input('Digite sua receita mensal:')
 despesas = input('Digite suas despesas mensais:')
 
 try:
@@ -30,3 +73,5 @@ elif res > 0 and res < 1000:
 
 else:
     print('Relatório: Situação financeira negativa!!!')
+
+'''
